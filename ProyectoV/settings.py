476c42e3060d94600c9204f54ccd6a78246829d2
Britney Collazos <alebcg1611@gmail.com>
2025-12-15 11:22:53 -0300

    Fix SQLite database for Render

--- ProyectoV/settings.py
+++ ProyectoV/settings.py
@@ -78,14 +78,12 @@ WSGI_APPLICATION = 'ProyectoV.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'conecta_vida',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PASSWORD': 'admin',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
